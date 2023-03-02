b9e09bdc19cbe4fa00a7585735e882846e9d797c
J R <mrjacobry@gmail.com>
2023-03-01 19:11:44 -0500

    added staveNote gen functions and exports

--- canvas.js
+++ canvas.js
@@ -1,18 +1,37 @@
-const { Factory, EasyScore, System } = Vex.Flow;
-const vf = new Factory({
-  renderer: { elementId: "output", width: 500, height: 200 },
-});
+const { Renderer, Stave, Voice, Formatter, Tables } = Vex.Flow;
+import { genBarNotes } from "./noteGenerator.js";
 
-const score = vf.EasyScore();
-const system = vf.System();
-notes = "C#5/q, B4/q, A4/q, G#4";
-system
-  .addStave({
-    voices: [
-      score.voice(score.notes(notes, { stem: "up" })),
-      score.voice(score.notes("C#4/h, C#4", { stem: "down" })),
-    ],
-  })
-  .addClef("treble")
-  .addTimeSignature("4/4");
-vf.draw();
+// Create an SVG renderer and attach it to the DIV element named "boo".
+const div = document.getElementById("output");
+const renderer = new Renderer(div, Renderer.Backends.SVG);
+
+// Configure the rendering context.
+renderer.resize(850, 808);
+const context = renderer.getContext();
+
+// Create a stave of width 400 at position 10, 40 on the canvas.
+const stave = new Stave(10, 40, 800);
+
+// Add a clef and time signature.
+stave.addClef("treble").addTimeSignature("4/4");
+
+// Connect it to the rendering context and draw!
+stave.setContext(context).draw();
+
+// Create the notes
+const notes = genBarNotes();
+
+// Create a voice in 4/4 and add above notes
+const voices = [
+  new Voice({
+    num_beats: 4,
+    beat_value: 4,
+  }).addTickables(notes),
+];
+// Format and justify the notes to 400 pixels.
+new Formatter().joinVoices(voices).format(voices, 350);
+
+// Render voice
+voices.forEach(function (v) {
+  v.draw(context, stave);
+});
--- noteGenerator.js
+++ noteGenerator.js
@@ -1,3 +1,4 @@
+const { StaveNote } = Vex.Flow;
 // list of valid notes
 const notesList = [
   "C",
@@ -15,29 +16,83 @@ const notesList = [
 ];
 
 const durationList = ["w", "h", "q"];
+const barDurationList = ["qqqq", "hh", "qqh", "hqq", "w"];
 
 function randomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
 }
-function genNoteSequence(sequenceLen, max = false) {
+export function genNoteSequence(sequenceLen, max = false, duration = "") {
   // if max  E0-B5, otherwise  E0-A2
-  noteSeq = [];
-  for (i = 0; i < sequenceLen; i++) {
+  let noteSeq = [];
+  for (let i = 0; i < sequenceLen; i++) {
     let note = { name: "", pitch: "", duration: "" };
     note.name = notesList[randomInt(0, notesList.length)];
-    if (note.name === "A") {
-      note.pitch = randomInt(0, 3).toString(); // 0-2 pitch
+    note.pitch = randomInt(4, 6).toString();
+    if (duration) {
+      note.duration = duration[i]; // duration is a 4 char string that tells which duration to make current note
     } else {
-      note.pitch = randomInt(0, 2).toString(); // 0-1 pitch
+      note.duration = durationList[randomInt(0, durationList.length)];
     }
-    note.duration = durationList[randomInt(0, durationList.length)];
     noteSeq.push(note);
   }
   return noteSeq;
 }
 
-let seq = genNoteSequence(1000);
-console.log(seq);
-function genNotes(noteSeq) {}
+export function genBar(random = true, durationType = 0) {
+  let duration = "";
+  if (random) {
+    const rnd = randomInt(0, 4);
+    duration = barDurationList[rnd];
+  } else {
+    dur = barDurationList[durationtype];
+  }
+
+  return genNoteSequence(duration.length, false, duration);
+}
+
+// const notes = [
+//   // A quarter-note C.
+//   new StaveNote({ keys: ["c/4"], duration: "q" }),
+
+//   // A quarter-note D.
+//   new StaveNote({ keys: ["d/4"], duration: "q" }),
+
+//   // A quarter-note rest. Note that the key (b/4) specifies the vertical
+//   // position of the rest.
+//   new StaveNote({ keys: ["b/4"], duration: "qr" }),
+
+//   // A C-Major chord.
+//   new StaveNote({ keys: ["c/4", "e/4", "g/4"], duration: "q" }),
+// ];
+
+export function genNotes(noteSeq) {
+  let notes = [];
+  noteSeq.forEach((note) => {
+    const name = note.name;
+    const pitch = note.pitch;
+    const duration = note.duration;
+    notes.push(
+      new StaveNote({ keys: [`${name}/${pitch}`], duration: `${duration}` })
+    );
+  });
+  return notes;
+}
+
+export function genBarNotes(num_bars = 1) {
+  let notes = [];
+  let noteSeq = [];
+  for (let i = 0; i < num_bars; i++) {
+    noteSeq.push(...genBar());
+  }
+  noteSeq.forEach((note) => {
+    const name = note.name;
+    const pitch = note.pitch;
+    const duration = note.duration;
+    notes.push(
+      new StaveNote({ keys: [`${name}/${pitch}`], duration: `${duration}` })
+    );
+  });
+  return notes;
+}
