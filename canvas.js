73f2187b982ea89dbc88e6ccba4cd6b19ac55b98
hydrol0x <mrjacobry@gmail.com>
2023-03-31 15:43:30 -0400

    centered and fixed cutoff for staffs

--- canvas.js
+++ canvas.js
@@ -4,11 +4,12 @@ import { genBarNotes, genBarStave } from "./noteGenerator.js";
 
 const viewportHeight = window.innerHeight;
 const viewportWidth = window.innerWidth;
+const RENDERER_WIDTH = viewportWidth * 0.85; // otherwise gets slightly cut off
 
 const div = document.getElementById("output");
 const renderer = new Renderer(div, Renderer.Backends.SVG);
 // Configure the rendering context.
-renderer.resize(viewportWidth * 0.98, window.innerHeight * 0.75);
+renderer.resize(RENDERER_WIDTH, window.innerHeight * 0.75);
 const context = renderer.getContext();
 
 const clearStaff = (staff) => {
@@ -19,11 +20,18 @@ const clearStaff = (staff) => {
   });
 };
 
+const parentWidth = (elem) => {
+  return elem.parentElem.clientWidth;
+};
+
+const X_POS = 0;
+const Y_POS = 0;
+const BAR_WIDTH = RENDERER_WIDTH / 2.01; // in order to not overflow page
 const generateNotes = () => {
   // context.rect(x, y, width, height, { stroke: "none", fill: "white" });
   // context.rect(0, 0, 100, 100, { stroke: "none", fill: "red" });
   const bars = genBarNotes(6);
-  const staves = genBarStave(context, 0, 0, 625, bars);
+  const staves = genBarStave(context, X_POS, Y_POS, BAR_WIDTH, bars);
 };
 
 generateNotes();
