01795aaf86e839df9475b01b897cae7e3637c401
hydrol0x <mrjacobry@gmail.com>
2023-03-30 23:36:10 -0400

    Added a generate button to re-generate staff w/out page reload

--- canvas.js
+++ canvas.js
@@ -1,46 +1,35 @@
+// TODO: switch to NPM package, add VexChords and VexTab
 const { Renderer, Stave, StaveNote, Voice, Formatter, Tables } = Vex.Flow;
 import { genBarNotes, genBarStave } from "./noteGenerator.js";
 
-// Create an SVG renderer and attach it to the DIV element named "boo".
+const viewportHeight = window.innerHeight;
+const viewportWidth = window.innerWidth;
+
 const div = document.getElementById("output");
 const renderer = new Renderer(div, Renderer.Backends.SVG);
-
 // Configure the rendering context.
-renderer.resize(2000, 1000);
+renderer.resize(viewportWidth * 0.98, window.innerHeight * 0.75);
 const context = renderer.getContext();
 
-// Create a stave of width 400 at position 10, 40 on the canvas.
-// const stave = new Stave(10, 40, 500);
+const clearStaff = (staff) => {
+  // staff is just the HTML element that contains staff svg
+  context.rect(0, 0, viewportWidth, viewportHeight, {
+    stroke: "none",
+    fill: "white",
+  });
+};
 
-// Add a clef and time signature.
-// stave.addClef("treble").addTimeSignature("4/4");
+const generateNotes = () => {
+  // context.rect(x, y, width, height, { stroke: "none", fill: "white" });
+  // context.rect(0, 0, 100, 100, { stroke: "none", fill: "red" });
+  const bars = genBarNotes(6);
+  const staves = genBarStave(context, 0, 0, 625, bars);
+};
 
-// Connect it to the rendering context and draw!
-// stave.setContext(context).draw();
+generateNotes();
 
-// Create the notes
-const bars = genBarNotes(6);
-const staves = genBarStave(context, 0, 0, 600, bars);
-// const notes = [
-//   // A quarter-note C.
-//   new StaveNote({ keys: ["c/4"], duration: "q" }),
-
-//   new StaveNote({ keys: ["d/4"], duration: "q" }),
-//   new StaveNote({ keys: ["d/4"], duration: "q" }),
-//   new StaveNote({ keys: ["b/4"], duration: "qr" }),
-
-//   // new measure
-//   new Vex.Flow.BarNote(),
-
-//   new StaveNote({ keys: ["c/4"], duration: "q" }),
-//   new StaveNote({ keys: ["d/4"], duration: "q" }),
-//   new StaveNote({ keys: ["b/4"], duration: "qr" }),
-//   new StaveNote({ keys: ["d/4"], duration: "q" }),
-// ];
-
-// Create a voice in 4/4 and add above notes
-
-// Render voice
-// voice.forEach(function (v) {
-//   v.draw(context, stave);
-// });
+const generatorButton = document.getElementById("generate-button");
+generatorButton.onclick = (e) => {
+  clearStaff(div);
+  generateNotes();
+};
